c436cf6fc6a1a49edccf3921928165b7984057b1
mike <mvteplitsky@yahoo.com>
2024-04-26 11:31:10 -0400

    dev

---
+++ src/main/java/com/aegisql/multifunction/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * This module contains extensions to Java functional interfaces
+ * <p>
+ *
+ * Copyright (C) 2024, AEGIS DATA SOLUTIONS
+ *
+ * @since 1.0
+ * @author Mikhail Teplitskiy
+ * @version 1.0
+ */
+package com.aegisql.multifunction;
